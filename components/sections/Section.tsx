106699080a187111d99e20544e5b254d04317d57
alic3 <alic3dev@gmail.com>
2023-10-18 12:27:06 -0400

    Allow custom classes on sections and containers

--- components/sections/Section.tsx
+++ components/sections/Section.tsx
@@ -6,11 +6,24 @@ import styles from './Section.module.scss'
 
 export default function Section({
   name,
+  className,
+  containerClassName,
   children,
-}: React.PropsWithChildren<{ name: ValidLocation }>): JSX.Element {
+}: React.PropsWithChildren<{
+  name: ValidLocation
+  className?: string
+  containerClassName?: string
+}>): JSX.Element {
   return (
-    <section id={name !== 'home' ? name : undefined} className={styles.section}>
-      <div className={styles['section-container']}>{children}</div>
+    <section
+      id={name !== 'home' ? name : undefined}
+      className={`${styles.section} ${className || ''}`}
+    >
+      <div
+        className={`${styles['section-container']} ${containerClassName || ''}`}
+      >
+        {children}
+      </div>
     </section>
   )
 }
