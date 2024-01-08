d57b12da00d720ca472d030972d1e14a60f5ff89
alic3 <alic3dev@gmail.com>
2024-01-08 18:01:18 -0500

    Fix build

--- app/tarot/_page.tsx
+++ app/tarot/_page.tsx
@@ -1,19 +1,11 @@
 import React from 'react'
 import type { Metadata } from 'next'
 
-import { TarotDeck } from '@/utils/TarotDeck'
-
 export const metadata: Metadata = {
-  title: 'T🂡A🂱R🃁0🃑T',
+  title: '🂡🂱🃁🃑',
   description: '🂡🂱🃁🃑T0🂡🂱🃁🃑R🂡🂱🃁🃑4T🂡🂱🃁🃑',
 }
 
 export default function TarotPage(): JSX.Element {
-  const tarotDeck = new TarotDeck()
-
-  // console.log(tarotDeck)
-  // let c
-  // while ((c = tarotDeck.drawRandomly())) console.log(c.toString())
-
-  return <main>🂡🂱🃁🃑</main>
+  return <main />
 }
