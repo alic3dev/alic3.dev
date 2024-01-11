d34f317124e45afa8482a91ca4527b15e03c7ece
alic3 <alic3dev@gmail.com>
2024-01-11 18:40:22 -0500

    Add Project component

---
+++ components/Project.tsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { FaGithub, FaExternalLinkAlt } from 'react-icons/fa'
+
+import Link from 'next/link'
+
+import styles from './Project.module.scss'
+
+export function Project({
+  title,
+  links = {},
+  description,
+  visual,
+}: {
+  title: string
+  links?: {
+    external?: string
+    github?: string
+  }
+  description: JSX.Element | JSX.Element[]
+  visual: JSX.Element
+}): JSX.Element {
+  return (
+    <article className={styles.project}>
+      <figure className={styles.visualization}>{visual}</figure>
+
+      <div className={styles.info}>
+        <div>
+          <h3 className={styles['project-title']}>{title}</h3>
+
+          <nav className={styles.links}>
+            {links.external && links.external.startsWith('/') ? (
+              <Link href={links.external} aria-label="Open">
+                <FaExternalLinkAlt aria-label="Open" />
+              </Link>
+            ) : (
+              <a href={links.external}>
+                <FaExternalLinkAlt aria-label="Open" />
+              </a>
+            )}
+            {links.github && (
+              <a href={links.github}>
+                <FaGithub alria-label="Github" />
+              </a>
+            )}
+          </nav>
+        </div>
+
+        {Array.isArray(description) ? (
+          description.map((descriptionElement) =>
+            descriptionElement.type ? (
+              <div key={descriptionElement.key} className={styles.description}>
+                {descriptionElement}
+              </div>
+            ) : (
+              <p key={descriptionElement.key} className={styles.description}>
+                {descriptionElement}
+              </p>
+            ),
+          )
+        ) : (
+          <p className={styles.description}>{description}</p>
+        )}
+      </div>
+    </article>
+  )
+}
