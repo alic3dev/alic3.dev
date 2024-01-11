d9cec8275eef0de0e6cb4795516520d3d058f103
alic3 <alic3dev@gmail.com>
2024-01-11 18:42:42 -0500

    Add projects to Projects section

--- components/sections/ProjectsSection.tsx
+++ components/sections/ProjectsSection.tsx
@@ -2,83 +2,124 @@
 
 import React from 'react'
 import { GiBleedingEye } from 'react-icons/gi'
-// import { PiFaceMaskFill } from 'react-icons/pi'
 
 import Image from 'next/image'
 
 import { Section } from '@/components/sections/Section'
-import { BackgroundImages } from '../decorative'
+import { Project } from '@/components/Project'
 
 import styles from './ProjectsSection.module.scss'
 
 export function ProjectsSection(): JSX.Element {
   return (
     <Section name="projects" className={styles.section}>
-      {/* <BackgroundImages
-        images={[
-          '/_216f6363-270e-47cf-b032-4ef8e523fb76.jpeg',
-          '/_9ac79e92-1d59-4904-bbf3-85ce2d031d1e.jpeg',
-          '/_9b3162b1-e1a8-4fa6-b578-381be3d8ee87.jpeg',
-          '/_84b3790c-609b-4464-8e23-6d21c41c579d.jpeg',
-          '/_38162f49-6778-4b3b-abeb-07be3a7b0103.jpeg',
-          // '/_c42c778a-c0b4-43eb-a69f-983003c3fb02.jpeg',
-        ]}
-      /> */}
       <div className={styles['section-header']}>
         <h2>Personal Projects</h2>
       </div>
 
-      <div className={styles.project}>
-        <div className={styles.visualization}>
+      <Project
+        title="BLUEGREEN"
+        links={{
+          external: 'https://bluegreen.alic3.dev',
+          github: 'https://github.com/alic3dev/bluegreen',
+        }}
+        description={
+          <>
+            A collection of tools/modules for the generation and manipulation of
+            audio including <a href="https://bluegreen.alic3.dev/zer0">ゼロ</a>{' '}
+            - a DAW for our WebAudio API based library{' '}
+            <a href="https://github.com/alic3dev/zer0">ZER0</a>.
+          </>
+        }
+        visual={
           <Image
             className={styles.image}
-            src="/_be3eddb9-c9e1-4bc9-a8df-2ec87a5e03df.jpeg"
-            alt="BLUEGREEN"
-            width={1024}
-            height={1024}
+            src="/23u5i3.png"
+            alt="ZER0 screenshot"
+            width={3620}
+            height={2336}
+            quality={60}
           />
-        </div>
-        <div className={styles.info}>
-          <h3 className={styles['project-title']}>BLUEGREEN</h3>
-          <p className={styles.description}>
-            A collection of tools/modules for the generation and manipulation of
-            audio including a DAW for our WebAudio API based library ZER0.
-          </p>
-        </div>
-      </div>
+        }
+      />
 
-      <div className={styles.project}>
-        <div className={styles.info}>
-          <h3 className={styles['project-title']}>
-            Miscellaneous Magickal Tools
-          </h3>
-          <p className={styles.description}>Explore esotericism.</p>
-          <p className={styles.description}>
+      <Project
+        title="Nature of Code"
+        links={{
+          external: 'https://nature-of-code.alic3.dev/',
+          github: 'https://github.com/alic3dev/nature-of-code',
+        }}
+        description={
+          <>
+            This is an ongoing implementation of exercises from the book{' '}
+            <a href="https://natureofcode.com/">The Nature of Code</a> by Daniel
+            Shiffman.
+          </>
+        }
+        visual={
+          <Image
+            className={styles.image}
+            src="/j24ihg.png"
+            alt="Nature of Code screenshot"
+            width={3620}
+            height={2336}
+            quality={60}
+          />
+        }
+      />
+
+      <Project
+        title="Kana Totsugeki"
+        links={{
+          external: 'https://nature-of-code.alic3.dev/',
+          github: 'https://github.com/alic3dev/nature-of-code',
+        }}
+        description={
+          <>
+            A simple game to help learn the Japanese kana systems hiragana and
+            katakana through the repitition of time restricted recognization.
+          </>
+        }
+        visual={
+          <Image
+            className={styles.image}
+            src="/234uo9.png"
+            alt="Kana Totsugeki screenshot"
+            width={3620}
+            height={2336}
+            quality={60}
+          />
+        }
+      />
+
+      <Project
+        title="Miscellaneous Magickal Tools"
+        links={{
+          external: '/portal',
+          github:
+            'https://github.com/alic3dev/alic3.dev/blob/main/app/tarot/_page.tsx',
+        }}
+        description={[
+          <React.Fragment key={'waiting'}>
             We&apos;(re/ve) are._-been// waiting.
-          </p>
-          <ul className={styles.description}>
+          </React.Fragment>,
+          <ul key={'includes'}>
             <li>Tarot</li>
             <li>Gematria</li>
-            <li>d4c2792a72452846f75dc4893e851977cb133db3</li>
+            <li>
+              d4<strong>c</strong>2792<strong>a</strong>724<strong>52</strong>
+              846f75dc4893e
+              <strong>8</strong>51977cb<strong>1</strong>33d<strong>b</strong>3
+            </li>
             <li>More...</li>
-          </ul>
-        </div>
-
-        <div className={`${styles.visualization} ${styles['with-icons']}`}>
-          {/* <Image
-            className={styles.image}
-            src="/4a03107ee9f15ae9b82cedbaa4ee027f042f5d87.jpeg"
-            alt=""
-            width={1024}
-            height={1024}
-          /> */}
-
-          <GiBleedingEye className={styles.icon} />
-          {/* <div className={styles.mouth}>
-            <PiFaceMaskFill className={styles.icon} />
-          </div> */}
-        </div>
-      </div>
+          </ul>,
+        ]}
+        visual={
+          <div className={styles['icons-container']}>
+            <GiBleedingEye className={styles.icon} aria-hidden />
+          </div>
+        }
+      />
     </Section>
   )
 }
