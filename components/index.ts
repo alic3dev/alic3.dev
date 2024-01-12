8a97ba8ff8c2658c15b8f4b6f00bccd90b8e9fc7
alic3 <alic3dev@gmail.com>
2024-01-11 19:27:33 -0500

    Add Gematria to components index

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,8 @@
 export { Footer } from '@/components/Footer'
 export { Header } from '@/components/Header'
+
 export { Resume } from '@/components/Resume'
+
+export { Gematria } from '@/components/Gematria'
 export { Tarot } from '@/components/Tarot'
 export { Trip } from '@/components/Trip'
