ed07328b6528747abb80dcefa6e329156150620a
alic3 <alic3dev@gmail.com>
2024-03-31 16:26:34 -0400

    Fix terms metadata

--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -1,5 +1,12 @@
+import { Metadata } from 'next'
+
 import { Footer, Header } from '@/components'
 import { LegalPageWrapper, TermsAndConditions } from '@/components/legal'
+
+export const metadata: Metadata = {
+  title: 'Alic3.dev - Terms',
+  description: 'Terms and Conditions for alic3.dev',
+}
 
 export default function TermsPage(): JSX.Element {
   return (
