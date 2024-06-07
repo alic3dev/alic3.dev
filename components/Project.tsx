b678b530c3697cdcb7bde69851cf87e2ebf80e7f
alic3 <alic3dev@gmail.com>
2024-06-07 15:00:38 -0400

    Refactor `Project` to use `CollapsibleItem`

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
-
 import type { Project } from '@/data/projects'
 
-import { LinksWithIcons } from './LinksWithIcons'
+import React from 'react'
+
+import { CollapsibleItem } from '@/components/CollapsibleItem'
+import { LinksWithIcons } from '@/components/LinksWithIcons'
+import { TechnologyList } from '@/components/TechnologyList'
 
 import styles from './Project.module.scss'
 
@@ -10,41 +12,43 @@ export function Project({
   title,
   links = {},
   description,
+  technologies,
   visual,
 }: Project): JSX.Element {
   return (
-    <article className={styles.project}>
-      <figure className={`${styles.visualization} ${styles.desktop}`}>
-        {visual}
-      </figure>
+    <CollapsibleItem
+      key={title}
+      id={`project|${title}`}
+      title={title}
+      subTitle={<LinksWithIcons links={links} />}
+    >
+      <div className={styles.project}>
+        <figure className={`${styles.visualization} ${styles.desktop}`}>
+          {visual}
+        </figure>
 
-      <div className={styles.info}>
-        <div>
-          <h3 className={styles['project-title']}>{title}</h3>
-
-          <LinksWithIcons links={links} />
-
+        <div className={styles.info}>
           <figure className={`${styles.visualization} ${styles.mobile}`}>
             {visual}
           </figure>
-        </div>
 
-        {Array.isArray(description) ? (
-          description.map((descriptionElement) =>
-            descriptionElement.type ? (
-              <div key={descriptionElement.key} className={styles.description}>
-                {descriptionElement}
-              </div>
-            ) : (
-              <p key={descriptionElement.key} className={styles.description}>
-                {descriptionElement}
-              </p>
-            ),
-          )
-        ) : (
-          <p className={styles.description}>{description}</p>
-        )}
+          <div className={styles.description}></div>
+          {Array.isArray(description) ? (
+            description.map(
+              (descriptionElement: JSX.Element): React.ReactNode =>
+                descriptionElement.type ? (
+                  <div key={descriptionElement.key}>{descriptionElement}</div>
+                ) : (
+                  <p key={descriptionElement.key}>{descriptionElement}</p>
+                ),
+            )
+          ) : (
+            <p>{description}</p>
+          )}
+
+          <TechnologyList technologies={technologies} />
+        </div>
       </div>
-    </article>
+    </CollapsibleItem>
   )
 }
